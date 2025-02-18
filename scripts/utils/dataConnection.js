91777b8d1985bb1bf7de6eb5851b960e1e59eda4
MariaValeria-Sandru <valy895@yahoo.com>
2025-02-18 13:13:20 +0100

    modifier meta

--- scripts/utils/dataConnection.js
+++ scripts/utils/dataConnection.js
@@ -1,25 +1,25 @@
 export async function getPhotographers() {
-    try{
-        const response = await fetch("./data/photographers.json");
+    try {
+        const response = await fetch("../data/photographers.json");
         /* attendre la résolution de la promesse  */
-        const data = await response.json(); 
-        return data.photographers; /* récupération tableau de données photographers  */ 
+        const data = await response.json();
+        return data.photographers; /* récupération tableau de données photographers  */
     }
-    catch(err) {
+    catch (err) {
         /* attrape les erreurs à la fois dans fetch et photographers.json */
-        console.log("Une erreur se produit :", err);   
-    }       
+        console.log("Une erreur se produit :", err);
+    }
 }
 
 export async function getMedia() {
-    try{
+    try {
         const response = await fetch("./data/photographers.json");
         /* attendre la résolution de la promesse  */
-        const data = await response.json(); 
-        return data.media; /* récupération tableau de données photographers  */ 
+        const data = await response.json();
+        return data.media; /* récupération tableau de données photographers  */
     }
-    catch(err) {
+    catch (err) {
         /* attrape les erreurs à la fois dans fetch et photographers.json */
-       console.log("Une erreur se produit :", err);
-    }       
+        console.log("Une erreur se produit :", err);
+    }
 }
